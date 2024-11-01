1b55deb076241140d7fcec3a734677f4241ea8e4
andreimama <132239794+andreimama@users.noreply.github.com>
2024-11-01 18:37:15 +1300

    Update Controller.java

--- src/languagelearner/Controller.java
+++ src/languagelearner/Controller.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package languagelearner;
 
 import java.awt.event.ActionEvent;
@@ -10,76 +6,75 @@ import javax.swing.JComboBox;
 
 /**
  *
- * @author franc
- */
+ * @author mamar
+ * 
+ * **/
+//Controller class managues user interactions and updates view and model in MVC
 public class Controller implements ActionListener {
+
+    //Fields to store references to view and model
     private MenuPanel menuPanel;
     private EasyPanel easyPanel;
     private LearnPanel learnPanel;
     private Model model;
+    private AdminPanel admin;
 
-    public Controller(MenuPanel menuPanel, EasyPanel easyPanel, LearnPanel learnPanel, Model model) {
+    //Constructor
+    public Controller(MenuPanel menuPanel, EasyPanel easyPanel, LearnPanel learnPanel, Model model, AdminPanel admin) {
         this.menuPanel = menuPanel;
         this.easyPanel = easyPanel;
         this.learnPanel = learnPanel;
         this.model = model;
+        this.admin = admin;
         
+        //Adds ActionListeners to the view components
         this.menuPanel.addActionListener(this);
         this.easyPanel.addActionListener(this);
         this.learnPanel.addActionListener(this);
+        this.admin.addActionListener(this);
         this.model.addListener(learnPanel);
         this.model.addListener(easyPanel);
+        this.model.addListener(admin);
     }
-
+    
+    //Handles events generated from the user interactions
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Button clicked!");
+        //If event source is Combobox then it sets language flag to either false(Tagalog) or true(Afrikaans)
         if (e.getSource() instanceof JComboBox) {
-        String command = (String) ((JComboBox<?>) e.getSource()).getSelectedItem();
-        if ("Tagalog".equals(command)) {
-            setLanguageFlag(false);
-            System.out.println("Current Language: " + model.getCurrentLanguage());
-        } else if ("Afrikaans".equals(command)) {
-            setLanguageFlag(true);
-            System.out.println("Current Language: " + model.getCurrentLanguage());
-        }
-    } else {
-        String command = e.getActionCommand();
-        System.out.println("Action:" + command);
-        switch (command) {
-            case "Easy Panel":
-                menuPanel.getMainFrame().switchTo("Easy Panel");
-                break;
-            case "Menu Panel":
-                easyPanel.getMainFrame().switchTo("Menu Panel");
+            String command = (String) ((JComboBox<?>) e.getSource()).getSelectedItem();
+            if ("Tagalog".equals(command)) {
+                setLanguageFlag(false); //Sets to Tagalog
                 
-                break;
-            case "Learn Panel":
-                learnPanel.getMainFrame().switchTo("Learn Panel");
-                break;
-           /* case "Tagalog":
-                setLanguageFlag(false);
-                System.out.println(model.getCurrentLanguage());
-                break;
-            case "Afrikaans":
-                setLanguageFlag(true);
-                System.out.println(model.getCurrentLanguage());
-                break; 8*/
-                /* case "Quit":
-                model.quitGame();
-                break; */
-            default:
+            } else if ("Afrikaans".equals(command)) {
+                setLanguageFlag(true); //Sets to Afrikaans
                 
-                break;
-        }
-    }
-    }
+            }
+        } else {
+            //If event source is button clicks, and switch views based on Action command
+            String command = e.getActionCommand();
+            
+            switch (command) {
+                case "Easy Panel":
+                    menuPanel.getMainFrame().switchTo("Easy Panel");
+                    break;
+                case "Menu Panel":
+                    easyPanel.getMainFrame().switchTo("Menu Panel");
 
+                    break;
+                case "Learn Panel":
+                    learnPanel.getMainFrame().switchTo("Learn Panel");
+                    break;
+                case "Admin Panel":
+                    admin.getMainFrame().switchTo("Admin Panel");
+                default:
+
+                    break;
+            }
+        }
+    }
+    //Sets language flag in model and updates it,
     public void setLanguageFlag(boolean lang) {
         model.updateLanguageFlag(lang);
-
     }
-  
 }
-
-
