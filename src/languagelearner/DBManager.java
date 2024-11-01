2098c3d46561405840ab308afa5550454eaa5203
andreimama <132239794+andreimama@users.noreply.github.com>
2024-11-01 18:37:46 +1300

    Update DBManager.java

--- src/languagelearner/DBManager.java
+++ src/languagelearner/DBManager.java
@@ -4,33 +4,33 @@
  */
 package languagelearner;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 /**
  *
  * @author franc
  */
+//Class for managing database and executes SQL Queries
 public final class DBManager {
-
+    //Database credentials
     private static final String USER_NAME = "Group99"; //DB username
     private static final String PASSWORD = "Ass2Lang"; //DB password
     private static final String URL = "jdbc:derby://localhost:1527/Phrases&Facts";  //url of the DB host
-
+    
+    //Connection object to interact with database
     Connection conn;
-
     public DBManager() {
         establishConnection();
     }
-
+    
     public Connection getConnection() {
         return this.conn;
     }
-
-    //Establish connection to DataBase
+    //Establish connection to DataBase if one does not already exist
     public void establishConnection() {
         if (this.conn == null) {
             try {
@@ -41,7 +41,7 @@ public final class DBManager {
             }
         }
     }
-
+    //Closes database connection
     public void closeConnections() {
         if (conn != null) {
             try {
@@ -51,12 +51,10 @@ public final class DBManager {
             }
         }
     }
-
+    //Executes an Update query on the database 
     public void updateDB(String sql) {
-
         Connection connection = this.conn;
         Statement statement = null;
-        ResultSet resultSet = null;
         try {
             statement = connection.createStatement();
             statement.executeUpdate(sql);
@@ -64,7 +62,7 @@ public final class DBManager {
             System.out.println(ex.getMessage());
         }
     }
-
+    //Queries the database and returns The queryt result
     public ResultSet getFromDB(String sql) {
         Connection connection = this.conn;
         Statement statement = null;
@@ -77,7 +75,7 @@ public final class DBManager {
         }
         return resultSet;
     }
-
+    //Retrievse a specific phrase through using an inedx.
     private String getPhrase(String tableName, String columnName, int index) {
         String phrase = null;
         String query = "SELECT " + columnName + " FROM " + tableName + " ORDER BY ID OFFSET " + index + " ROWS FETCH NEXT 1 ROWS ONLY";
@@ -94,7 +92,7 @@ public final class DBManager {
         }
         return phrase;
     }
-
+    //Get all Phrases from a specific table & column
     public String[] getAllPhrases(String tableName, String columnName) {
         int count = 0;
         String countQuery = "SELECT COUNT(*) FROM " + tableName;
@@ -118,4 +116,3 @@ public final class DBManager {
         return phrases;
     }
 }
-
